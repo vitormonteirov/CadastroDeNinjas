142ca0755005100c99b4d77dfdf159fcb35fa789
Vitor Monteiro <vitormv725@gmail.com>
2026-09-11 16:26:25 -0300

    first commit

---
+++ src/test/java/com/example/java10x/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java10x.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
