f0d1f7d7c99203952272b280f373ebfa6817a560
Vitor Monteiro <vitormv725@gmail.com>
2026-09-11 20:47:04 -0300

    adicionado dependencias e atributos ao model

--- src/main/java/com/example/java10x/CadastroDeNinjas/NinjaModel.java
+++ src/main/java/com/example/java10x/CadastroDeNinjas/NinjaModel.java
@@ -1,45 +1,22 @@
 package com.example.java10x.CadastroDeNinjas;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class NinjaModel {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String nome;
     private String email;
+    private String rank;
+    private String skill;
     private int idade;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(int idade, String email, String nome) {
-        this.idade = idade;
-        this.email = email;
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
