512c5302c5051e810a9f572fb656ac3f42401046
Vitor Monteiro <vitormv725@gmail.com>
2026-09-11 20:07:02 -0300

    criando ninja model sem JPA e lombok

---
+++ src/main/java/com/example/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,45 @@
+package com.example.java10x.CadastroDeNinjas;
+
+
+public class NinjaModel {
+
+
+    private long id;
+
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(int idade, String email, String nome) {
+        this.idade = idade;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
