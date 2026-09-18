db58c78cd86b5ecbfaff0bdf570683e883d6ed58
Vitor Monteiro <vitormv725@gmail.com>
2026-09-18 17:19:14 -0300

    chore: luta dificil para configurar o .env e o properties

--- src/main/java/com/example/java10x/CadastroDeNinjas/Ninja/NinjaRepository.java
+++ src/main/java/com/example/java10x/CadastroDeNinjas/Ninja/NinjaRepository.java
@@ -3,4 +3,5 @@ package com.example.java10x.CadastroDeNinjas.Ninja;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
 }
