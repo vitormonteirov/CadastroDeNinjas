bed73d506cf860502e75ac800a3b411dd6ff416b
Vitor Monteiro <vitormv725@gmail.com>
2026-09-23 19:49:51 -0300

    feat: adicionando a classe Controller do Ninja

--- src/main/java/com/example/java10x/CadastroDeNinjas/Ninja/NinjaController.java
+++ src/main/java/com/example/java10x/CadastroDeNinjas/Ninja/NinjaController.java
@@ -1,17 +1,51 @@
 package com.example.java10x.CadastroDeNinjas.Ninja;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class NinjaController {
+
+    @Autowired
+    private NinjaRepository ninjaRepository;
 
     @GetMapping("/bemvindo")
     public String BemVindo(){
         return "Bem Vindo";
     }
 
+    //Adicionar ninja (CREATE)
+    @PostMapping("/adicionar")
+    public String criarNinja(){
+        return "Adicionando ninja";
+    }
 
+    //Mostrar ninjas (READ)
+    @GetMapping("/listar")
+    public List<NinjaModel> listar(){
+        return ninjaRepository.findAll();
+    }
+
+    //Mostrar ninjas por ID (READ)
+    @GetMapping("/listaID")
+    public String listarID(){
+        return "Listando ninjas por ID";
+    }
+
+    //Alterar dados do ninja (UPDATE)
+    @PutMapping("/attNinja")
+    public String atualizarNinja(){
+        return "Atualizando ninja";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarNinja")
+    public String deletarNinja(){
+        return "Deletando ninja";
+    }
 }
